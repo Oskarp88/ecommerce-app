ad64599f7ad52bbaa60d6757588f99cf56466e2c
Oskarp88 <oskyburgos81@gmail.com>
2023-05-01 19:43:34 -0500

    category frontend

--- client/src/pages/Admin/CreateCategorias.js
+++ client/src/pages/Admin/CreateCategorias.js
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminMenu from '../../components/Layout/AdminMenu'
 import Layout from '../../components/Layout/Layout'
+import axios from 'axios';
+import { Button, Modal } from 'antd';
+import { toast } from 'react-toastify';
+import CategoryForm from '../../components/Form/CategoryForm';
+import { GiTrumpet } from 'react-icons/gi';
 
 const CreateCategorias = () => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updateName, setUpdateName] = useState('');
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const {data} = await axios.post(`${process.env.REACT_APP_API}/api/v1/category/create-category`, {name});
+      if(data?.success){
+        toast.success(`${name} is created`);
+        getAllCategory();
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong in input form');
+    }
+  }
+
+  //get all cat
+  const getAllCategory = async () => {
+    try {
+       const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/get-categories`);
+       if(data.success){
+          setCategories(data.category);
+       }
+      } catch (error) {
+      console.log(error);
+      toast.error(`Something wwent wrong in getting category`);
+    }
+  }
+
+  useEffect(()=>{
+    getAllCategory();
+    
+  },[]);
+
+ //update
+  const handleUpdate = async(e) =>{
+    e.preventDefault();
+    try {
+      const {data} = await axios.put(`${process.env.REACT_APP_API}/api/v1/category/update-category/${selected._id}`, {name:updateName});
+      if(data.success){
+        toast.success(`${updateName} is updated`);
+        setSelected(null);
+        setUpdateName('');
+        setVisible(false);
+        getAllCategory();
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error('Somtihing went wrong')
+    }
+  }
+
+  //DELETE
+  const handleDelete = async(id) =>{
+    try {
+      const {data} = await axios.delete(`${process.env.REACT_APP_API}/api/v1/category/delete-category/${id}`);
+      if(data.success){
+        toast.success(`${data.message}`);
+        getAllCategory();
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error('Somtihing went wrong')
+    }
+  }
   return (
     <Layout title={'Dashboard - Create Category'}>
         <div className='container-fluid m-3 p-3'>
@@ -11,7 +89,49 @@ const CreateCategorias = () => {
                 <AdminMenu/>
                 </div>
                 <div className='col-md-9'>
-                <h1>Create Category</h1>
+                  <h1>Manage Category</h1>
+                  <div className='p-3 w-50'>
+                    <CategoryForm 
+                       handleSubmit={handleSubmit}
+                       value={name}
+                       setValue={setName}
+                    />
+                  </div>
+                  <div className='w-75'>
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">Name</th>
+                          <th scope="col">Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>                       
+                           {categories?.map(c=> (
+                            <>
+                               <tr>
+                                <td key={c._id}>{c.name}</td>
+                                <td>
+                                  <button className='btn btn-primary ms-2' onClick={() => {setVisible(true); setUpdateName(c.name); setSelected(c); console.log(c._id)}}>Edit</button>
+                                  <button className='btn btn-danger ms-2' onClick={()=>handleDelete(c._id)}>Delete</button>
+                                </td>
+                               </tr>
+                            </>
+                            
+                           ))}
+                      </tbody>
+                    </table>
+                  </div>
+                  <Modal 
+                    onCancel={()=> setVisible(false)} 
+                    footer={null} 
+                    visible={visible}
+                  >
+                    <CategoryForm
+                       value={updateName} 
+                       setValue={setUpdateName}
+                       handleSubmit={handleUpdate}
+                    />
+                  </Modal>
                 </div>
             </div>
         </div>
--- controllers/productController.js
+++ controllers/productController.js
@@ -41,3 +41,131 @@ export const createProductController = async(req, res) => {
        })
    }
 }
+
+export const updateProductController = async(req, res) => {
+    try {
+        const {name,slug,description,price,category,quantify,shipping} = req.fields;
+        const {photo} = req.files;
+ 
+        switch(true){
+          case !name:
+             return res.status(500).send({error: 'Name is Required'})
+         case !description:
+             return res.status(500).send({error: 'Description is Required'})
+         case !price:
+             return res.status(500).send({error: 'Price is Required'})
+         case !category:
+             return res.status(500).send({error: 'Category is Required'})
+         case !quantify:
+             return res.status(500).send({error: 'Quantify is Required'})
+         case photo && photo.size > 1000000:
+             return res.status(500).send({error: 'Shipping is Required and should be less then 1mb'})
+         }
+        const products = await productModel.findByIdAndUpdate(req.params.pid,{
+            ...req.fields, 
+            slug: slugify(name)
+        },
+        {
+            new: true
+        });
+
+        if(photo){
+           products.photo.data = fs.readFileSync(photo.path);
+           products.photo.contentType = photo.type;
+        }
+        await products.save();
+        res.status(201).send({
+         success:true,
+         message:'Product updated Successfully',
+         products
+        })
+     } catch (error) {
+        console.log(error);
+        res.status(500).send({
+         success:false,
+         error,
+         message:'Error in update product'
+        })
+    }
+}
+
+export const getProductController = async(req, res) => {
+    try {
+        const product = await productModel
+           .find({})
+           .populate('category')
+           .select('-photo')
+           .limit(12)
+           .sort({ createdAt: -1});
+        res.status(200).send({
+            success: true,
+            counTotal: product.length,
+            message: 'ALLproducts',
+            product
+        })
+    } catch (error) {
+        console.log(error);
+       res.status(500).send({
+        success:false,
+        error,
+        message:'Error getting product'
+       })
+    }
+}
+
+export const getSingleProductController = async(req,res) => {
+    try {
+        const product = await productModel
+        .findOne({slug:req.params.slug})
+        .select('-photo')
+        .populate('category');
+        res.status(201).send({
+            success: true,
+            message: 'Single Product Fetched',
+            product
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message: 'Error while getling single product',
+            error
+        })
+    }
+}
+
+export const productPhotoController = async(req, res) => {
+    try {
+        const product = await productModel.findById(req.params.pid).select('photo');
+        if(product.photo.data){
+            res.set('Content-type', product.photo.contentType)
+            return res.status(200).send(product.photo.data);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message: 'Error while getting photo',
+            error
+        })
+    }
+}
+
+export const deleteProductController = async(req,res) => {
+    const { pid } = req.params;
+    try {
+        await productModel.findByIdAndDelete(pid).select('-photo');
+        res.status(200).send({
+            success:true,
+            message:'Product Delete successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message:'Error while deleting product',
+            error
+        })
+    }
+}
