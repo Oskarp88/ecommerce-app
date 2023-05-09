8d59b3dcec2bbcdc6207ed05a1cdad31edfc934b
Oscar Burgos <Oskyburgos81@gmail.com>
2023-05-09 10:49:33 -0500

    deploy-final
    Ecommerce-app

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoute.js';
 import productRoute from './routes/productRoute.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
@@ -20,14 +21,16 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(--dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoute);
 
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to ecommerce app</>")
+//rest api
+app.use('*', function(req,res){
+   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 const PORT = process.env.PORT;
