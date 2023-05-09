b6246f9936aa55939fe9f44fe3df1fe6d2697421
Oscar Burgos <Oskyburgos81@gmail.com>
2023-05-09 11:15:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoute.js';
 import productRoute from './routes/productRoute.js';
 import cors from 'cors';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 dotenv.config();
 
@@ -21,7 +22,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(--dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
